811bc88157aaa8b1a17f5696aea0d609cc28870e
Sharada Shehan <sharadashehan6@gmail.com>
2024-07-13 21:20:04 +0530

    technician maintenances routes

---
+++ app/(technician)/(maintenances)/(completed)/approved-maintenances.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { Text, View } from '@/components/Themed';
+
+export default function ApprovedMaintenancesScreen() {
+  const colorScheme = useColorScheme();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Approved Maintenances</Text>
+      <Text style={styles.title}>Maintenance 1</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
