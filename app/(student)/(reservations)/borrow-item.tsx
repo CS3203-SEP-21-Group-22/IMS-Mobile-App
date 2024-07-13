ca312c11f6c2e1cb7466b436d10eff63c8858365
Sharada Shehan <sharadashehan6@gmail.com>
2024-07-13 21:16:54 +0530

    student reservations handling routes

---
+++ app/(student)/(reservations)/borrow-item.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { Text, View } from '@/components/Themed';
+
+export default function ReservedItemsScreen() {
+  const colorScheme = useColorScheme();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Borrow Item</Text>
+      <Text style={styles.title}>QR Code</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
