48eac17236c2aabc17f1c1bf0c04ad55d999f289
Sharada Shehan <sharadashehan6@gmail.com>
2024-07-13 21:16:19 +0530

    student borrowed items handling routes

---
+++ app/(student)/(borrowed-items)/_layout.tsx
@@ -0,0 +1,58 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+import { useColorScheme } from '@/components/useColorScheme';
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+export const unstable_settings = {
+  initialRouteName: 'borrowed-items',
+};
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function BorrowedItemsLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../../../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <BorrowedItemsLayoutNav />;
+}
+
+function BorrowedItemsLayoutNav() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="borrowed-items" options={{ title: 'Borrowed Items' }} />
+        <Stack.Screen name="return-item" options={{ title: 'Return Item' }} />
+      </Stack>
+    </ThemeProvider>
+  );
+}
+
---
+++ app/(student)/(borrowed-items)/borrowed-items.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { Text, View } from '@/components/Themed';
+
+export default function BorrowedItemsScreen() {
+  const colorScheme = useColorScheme();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Items to be Returned</Text>
+      <Link href="/(student)/(borrowed-items)/return-item" asChild>
+          <Pressable>
+            {({ pressed }) => (
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text, opacity: pressed ? 0.5 : 1 }}>
+                Return Item 1
+              </Text>
+              )}
+          </Pressable>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
