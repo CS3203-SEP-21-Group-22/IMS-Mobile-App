695a40f1913435b72bfd1d835c3cec03eff64cd7
Sharada Shehan <sharadashehan6@gmail.com>
2024-07-13 21:19:30 +0530

    technician explore equipments routes

---
+++ app/(technician)/(explore-equipments)/view-item.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet, Pressable } from 'react-native';
+import { Link } from 'expo-router';
+import Colors from '@/constants/Colors';
+import { useColorScheme } from '@/components/useColorScheme';
+import { Text, View } from '@/components/Themed';
+
+export default function ViewItemScreen() {
+  const colorScheme = useColorScheme();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>View Items</Text>
+      <Link href="/(technician)/(explore-equipments)/reservations" asChild>
+          <Pressable>
+            {({ pressed }) => (
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text, opacity: pressed ? 0.5 : 1 }}>
+                View Reservations
+              </Text>
+              )}
+          </Pressable>
+      </Link>
+      <Link href="/(technician)/(explore-equipments)/maintenances" asChild>
+          <Pressable>
+            {({ pressed }) => (
+              <Text style={{ color: Colors[colorScheme ?? 'light'].text, opacity: pressed ? 0.5 : 1 }}>
+                View Maintenances
+              </Text>
+              )}
+          </Pressable>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
