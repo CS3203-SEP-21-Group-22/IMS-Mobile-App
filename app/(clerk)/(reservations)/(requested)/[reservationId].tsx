b8c00d212280d28a8d4f45d64cc95b4155a7b7c0
Sharada Shehan <sharadashehan6@gmail.com>
2024-07-16 08:17:57 +0530

    clerk requested reservations

---
+++ app/(clerk)/(reservations)/(requested)/[reservationId].tsx
@@ -0,0 +1,217 @@
+import { StyleSheet, Pressable, Image, TextInput, ImageBackground, ScrollView } from 'react-native';
+import { Link, useLocalSearchParams, router } from 'expo-router';
+import { Text, View } from '@/components/Themed';
+import BackgroundLayout from '@/components/BackgroundLayout';
+import ContentContainer from '@/components/ContentContainer';
+import MainHeader from '@/components/MainHeader';
+import ClerkReservationsHorizontalBar from '@/components/ClerkReservHorizontalBar';
+import ContentContainerHeader from '@/components/ContentContainerHeader';
+import SingleItemBackground from '@/components/SingleItemBackground';
+import SingleItemWithImage from '@/components/SingleItemWithImage';
+import { Dropdown } from 'react-native-element-dropdown';
+import { useState } from 'react';
+
+interface Reservation {
+  id: number;
+  name: string;
+  model: string;
+  lab: string;
+  user: string;
+  fromDate: string;
+  toDate: string;
+  requestedAt: string;
+  imageURL?: string | null;
+}
+
+interface Item {
+  id: number;
+  serialNumber: string;
+}
+
+const handleAccept = () => {
+  router.replace('/(clerk)/(reservations)/(requested)/items');
+}
+
+const handleReject = () => {
+  router.replace('/(clerk)/(reservations)/(requested)/items');
+}
+
+export default function ViewRequestedItemScreen() {
+  const { reservationId } = useLocalSearchParams<{ reservationId: string }>();
+  if (!reservationId) throw new Error('Missing equipmentId');
+  const reservation: Reservation = {
+    id: parseInt(reservationId),
+    name: '4-Port WiFi Router',
+    model: 'Cisco SRP541W',
+    lab: 'Network Lab',
+    user: 'John Doe',
+    fromDate: '2024-08-02 12:03',
+    toDate: '2024-08-02 12:03',
+    requestedAt: '2024-08-02 12:03',
+    imageURL: null,
+  };
+  const itemsList: Item[] = [
+    { id: 1, serialNumber: 'FOC1234X56Y' },
+    { id: 2, serialNumber: 'FOC1234X56Z' },
+    { id: 3, serialNumber: 'FOC1234X56A' },
+  ];
+  const [selectedItem, setSelectedItem] = useState<Item | null>(null);
+  return (
+    <BackgroundLayout>
+    <MainHeader title="Reservations" />
+    <ClerkReservationsHorizontalBar selectedIndex = {0} />
+    <ContentContainer>
+    <View style={styles.container}>
+      <ContentContainerHeader title="Item Request" />
+      <SingleItemBackground>
+        <ScrollView>
+        <SingleItemWithImage title={reservation.name} link={reservation.imageURL ?? 'equipment'}>
+          <Text style={styles.text}>
+            Model: {reservation.model}
+          </Text>
+          <Text style={styles.text}>
+            Lab: {reservation.lab}
+          </Text>
+          <View style={styles.textSeparator} />
+          <Text style={styles.text}>
+            Requested By: {reservation.user}
+          </Text>
+          <Text style={styles.text}>
+            From: {reservation.fromDate}
+          </Text>
+          <Text style={styles.text}>
+            To: {reservation.toDate}
+          </Text>
+          <View style={styles.textSeparator} />
+          <Text style={styles.text}>
+            Requested At: {reservation.requestedAt}
+          </Text>
+          <View style={styles.textSeparator} />
+          <Text style={styles.text}>
+            Assigned Item:
+          </Text>
+          <Dropdown
+            data={itemsList}
+            mode='modal'
+            search
+            searchPlaceholder='Search Item'
+            labelField="serialNumber"
+            valueField="id"
+            onChange={(item) => setSelectedItem(item)}
+            style={styles.dropdown}
+            placeholder={ selectedItem ? selectedItem.serialNumber : 'Select Item' }
+            placeholderStyle={styles.dropdownText}
+            selectedTextStyle={styles.dropdownText}
+          />
+          <TextInput
+            style={styles.multilineInput}
+            placeholder="Notes"
+            multiline
+          />
+          <View style={styles.textSeparator} />
+          <View style={styles.buttonsContainer}>
+            <View style={styles.button}>
+              <ImageBackground
+                source={require('@/assets/images/redBtn.webp')}
+                style={styles.buttonBackground}
+                borderRadius={12}
+              >
+                <Pressable onPress={handleReject}>
+                  <Text style={styles.buttonText}>
+                    Reject
+                  </Text>
+                </Pressable>
+              </ImageBackground>
+            </View>
+            <View style={styles.button}>
+              <ImageBackground
+                source={require('@/assets/images/blueBtn.webp')}
+                style={styles.buttonBackground}
+                borderRadius={12}
+              >
+                <Pressable onPress={handleAccept}>
+                  <Text style={styles.buttonText}>
+                    Assign
+                  </Text>
+                </Pressable>
+              </ImageBackground>
+            </View>
+          </View>
+        </SingleItemWithImage>
+        </ScrollView>
+      </SingleItemBackground>
+    </View>
+    </ContentContainer>
+    </BackgroundLayout>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+    width: '100%',
+  },
+  titleText: {
+    color:'white',
+    fontSize: 13,
+    fontWeight: 'bold',
+  },
+  text: {
+    color: 'white',
+    fontSize: 12,
+    marginBottom: '0.2%',
+  },
+  textSeparator: {
+    marginVertical: '1%',
+    height: 0.1,
+    width: '80%',
+    backgroundColor: 'transparent',
+  },
+  dropdown: {
+    marginTop: '2%',
+    marginBottom: '4%',
+    backgroundColor: 'white',
+    borderRadius: 8,
+    paddingHorizontal: '5%',
+    paddingVertical: '2%',
+    width: 200,
+  },
+  dropdownText: {
+    color: 'black',
+    fontSize: 13,
+    alignSelf: 'center',
+    paddingLeft: '3%',
+    paddingVertical: '1%',
+  },
+  multilineInput: {
+    backgroundColor: 'white',
+    width: 200,
+    height: 60,
+    borderRadius: 8,
+    paddingLeft: '3%',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginTop: '3%',
+    backgroundColor: 'transparent',
+  },
+  button: {
+    width: '45%',
+    backgroundColor: 'transparent',
+  },
+  buttonBackground: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    paddingTop: '4%',
+    paddingBottom: '6%',
+  },
+});
--- app/(clerk)/(reservations)/(requested)/items.tsx
+++ app/(clerk)/(reservations)/(requested)/items.tsx
@@ -1,24 +1,120 @@
-import { StyleSheet, Pressable } from 'react-native';
+import { StyleSheet, Pressable, FlatList } from 'react-native';
 import { Link } from 'expo-router';
-import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { Text, View } from '@/components/Themed';
+import BackgroundLayout from '@/components/BackgroundLayout';
+import ContentContainer from '@/components/ContentContainer';
+import MainHeader from '@/components/MainHeader';
+import ClerkReservationsHorizontalBar from '@/components/ClerkReservHorizontalBar';
+import ContentContainerHeader from '@/components/ContentContainerHeader';
+import ListItemBackground from '@/components/ListItemBackground';
+import ListItemWithImage from '@/components/ListItemWithImage';
 
-export default function ViewRequestedItemsScreen() {
-  const colorScheme = useColorScheme();
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>View Requested Items</Text>
-      <Link href="/(clerk)/(reservations)/(requested)/item" asChild>
+// 4-Port WiFi Router (Cisco SRP541W)
+// Lab : Network Lab
+// User : John Doe
+// Requested At 2024-08-02 12:03
+
+interface Reservation {
+  id: number;
+  name: string;
+  model: string;
+  lab: string;
+  user: string;
+  requestedAt: string;
+  imageURL?: string | null;
+}
+
+const ItemComponent: React.FC<{ item: Reservation }> = ({ item }) => (
+  <Link href={`/(clerk)/(reservations)/(requested)/${item.id}`} asChild>
           <Pressable>
             {({ pressed }) => (
-              <Text style={{ color: Colors[colorScheme ?? 'light'].text, opacity: pressed ? 0.5 : 1 }}>
-                Requested Item 1
-              </Text>
+              <ListItemBackground>
+                <ListItemWithImage link={item.imageURL ?? 'equipment'}>
+                <Text style={styles.titleText}>
+                  {item.name}
+                </Text>
+                <Text style={styles.text}>
+                  Model: {item.model}
+                </Text>
+                <Text style={styles.text}>
+                  Lab: {item.lab}
+                </Text>
+                <Text style={styles.text}>
+                  User: {item.user}
+                </Text>
+                <Text style={styles.text}>
+                  Requested At: {item.requestedAt}
+                </Text>
+                </ListItemWithImage>
+              </ListItemBackground>
               )}
           </Pressable>
-      </Link>
-    </View>
+  </Link>
+);
+
+export default function ViewRequestedItemsScreen() {
+  const reservations: Reservation[] = [
+    {
+      id: 1,
+      name: '4-Port WiFi Router',
+      model: 'Cisco SRP541W',
+      lab: 'Network Lab',
+      user: 'John Doe',
+      requestedAt: '2024-08-02 12:03',
+    },
+    {
+      id: 2,
+      name: '8-Port Ethernet Switch',
+      model: 'Cisco SG350-10P',
+      lab: 'Network Lab',
+      user: 'Jane Doe',
+      requestedAt: '2024-08-02 12:03',
+    },
+    {
+      id: 3,
+      name: '24-Port Ethernet Switch',
+      model: 'Cisco SG350-28P',
+      lab: 'Network Lab',
+      user: 'John Doe',
+      requestedAt: '2024-08-02 12:03',
+    },
+    {
+      id: 4,
+      name: '16-Port PoE Switch',
+      model: 'Cisco SG350-28P',
+      lab: 'Network Lab',
+      user: 'Jane Doe',
+      requestedAt: '2024-08-02 12:03',
+    },
+    {
+      id: 5,
+      name: '24-Port PoE Switch',
+      model: 'Cisco SG350-28P',
+      lab: 'Network Lab',
+      user: 'John Doe',
+      requestedAt: '2024-08-02 12:03',
+    }
+  ];
+  const colorScheme = useColorScheme();
+  return (
+    <BackgroundLayout>
+      <MainHeader title="Reservations" />
+      <ClerkReservationsHorizontalBar selectedIndex = {0} />
+      <ContentContainer>
+      <View style={styles.container}>
+        <ContentContainerHeader title="Requested Items" />
+        <FlatList
+            data={reservations}
+            renderItem={({ item }) => <ItemComponent item={item} />}
+            keyExtractor={(item) => item.id.toString()}
+            style={styles.flatList}
+            contentContainerStyle={{ alignItems: 'stretch', justifyContent: 'center', width: '100%', backgroundColor: 'transparent' }}
+            
+          />
+      </View>
+      </ContentContainer>
+    </BackgroundLayout>
   );
 }
 
@@ -27,14 +123,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'transparent',
+    width: '100%',
   },
-  title: {
-    fontSize: 20,
+  flatList: {
+    width: '100%',
+    backgroundColor: 'transparent',
+  },
+  titleText: {
+    color:'white',
+    fontSize: 13,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  text: {
+    color: 'white',
+    fontSize: 10,
   },
 });
+
