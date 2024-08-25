ac0bdb56976f4f225c0dd39061f7b79b17a49299
Sharada Shehan <sharadashehan6@gmail.com>
2024-08-25 11:48:44 +0530

    QR scanner for verifications

--- app/(clerk)/(reservations)/(reserved)/verify.tsx
+++ app/(clerk)/(reservations)/(reserved)/verify.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Pressable } from 'react-native';
+import { StyleSheet, Pressable, Linking, TouchableOpacity, Button, Alert } from 'react-native';
 import { Text, View } from '@/components/Themed';
 import { Link, router, useLocalSearchParams } from 'expo-router';
 import BackgroundLayout from '@/components/BackgroundLayout';
@@ -6,18 +6,99 @@ import ContentContainer from '@/components/ContentContainer';
 import MainHeader from '@/components/MainHeader';
 import ClerkReservationsHorizontalBar from '@/components/ClerkReservHorizontalBar';
 import ContentContainerHeader from '@/components/ContentContainerHeader';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import React, { useState } from 'react';
 
 export default function VerifyBorrowingItemScreen() {
   const { reservationId } = useLocalSearchParams<{ reservationId: string }>();
-  if (!reservationId) throw new Error('Missing equipmentId');
+  if (!reservationId) throw new Error('Missing reservationId');
+  const [scanned, setScanned] = useState(false);
+  const [scannedValue, setScannedValue] = useState('');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) return <View />;
+
+  if (!permission.granted) {
+      return (
+          <View style={styles.container}>
+              <Text style={{ textAlign: 'center' }}>
+                  Requires permission to access the camera
+              </Text>
+              <Button onPress={requestPermission} title='grant permission' />
+          </View>
+      );
+  }
+
+  const handleBarCodeScanned = ({ data }: { data: string }) => {
+      if (data) {
+          setScanned(true);
+          setScannedValue(data);
+      }
+  };
+
+  const handleResult = () => {
+      if (scannedValue) {
+          try {
+              const { verifyToken } = JSON.parse(scannedValue);
+              console.log(verifyToken);
+              return (
+                  <Text style={styles.resulttext}>
+                      {'Successfully, verified lending item'}
+                  </Text>
+              );
+          } catch (e) {
+              return (
+                  <Text style={styles.resulttext}>
+                      {'Invalid QR code. Please scan a valid QR code'}
+                  </Text>
+              );
+          }
+      }
+      return <Text style={styles.resulttext}>No QR code scanned</Text>;
+  }
+
   return (
     <BackgroundLayout>
     <MainHeader title="Reservations" />
     <ClerkReservationsHorizontalBar selectedIndex = {1} />
     <ContentContainer>
-    <View style={styles.container}>
-      <ContentContainerHeader title="Verify Borrowing Item" />
-    </View>
+      <View style={styles.container}>
+        <View style={styles.camerabox}>
+                    <CameraView
+                        barcodeScannerSettings={{
+                            barcodeTypes: [
+                                'aztec',
+                                'ean13',
+                                'ean8',
+                                'qr',
+                                'pdf417',
+                                'upc_e',
+                                'datamatrix',
+                                'code39',
+                                'code93',
+                                'itf14',
+                                'codabar',
+                                'code128',
+                                'upc_a',
+                            ],
+                        }}
+                        onBarcodeScanned={
+                            scanned ? undefined : handleBarCodeScanned
+                        }
+                        style={StyleSheet.absoluteFillObject}
+                    />
+                    {scanned && (
+                        <Button
+                            title={'Tap to Scan Again'}
+                            onPress={() => setScanned(false)}
+                        />
+                    )}
+        </View>
+        <View style={styles.textbox}>
+          {handleResult()}
+        </View>
+        <ContentContainerHeader title="Verify Borrowing Item" />
+      </View>
     </ContentContainer>
     </BackgroundLayout>
   );
@@ -40,4 +121,47 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+  camerabox: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+  },
+  resultlink: {
+      color: 'blue',
+      flexWrap: 'wrap',
+      fontFamily: 'monospace',
+      padding: 20,
+      textDecorationLine: 'underline',
+  },
+  resulttext: {
+      color: 'white',
+      flexWrap: 'wrap',
+      fontFamily: 'monospace',
+      padding: 20,
+  },
+  textbox: {
+      borderTopColor: 'blue',
+      borderTopWidth: 3,
+      alignItems: 'center',
+      flex: 1,
+      justifyContent: 'center',
+      width: '100%',
+  }
 });
--- app/(student)/(borrowed-items)/return-item.tsx
+++ app/(student)/(borrowed-items)/return-item.tsx
@@ -1,16 +1,106 @@
-import { StyleSheet, Pressable } from 'react-native';
-import { Link } from 'expo-router';
-import Colors from '@/constants/Colors';
-import { useColorScheme } from '@/components/useColorScheme';
+import { StyleSheet, Pressable, Button } from 'react-native';
+import { Link, router, useLocalSearchParams } from 'expo-router';
 import { Text, View } from '@/components/Themed';
+import BackgroundLayout from '@/components/BackgroundLayout';
+import ContentContainer from '@/components/ContentContainer';
+import MainHeader from '@/components/MainHeader';
+import ClerkReservationsHorizontalBar from '@/components/ClerkReservHorizontalBar';
+import ContentContainerHeader from '@/components/ContentContainerHeader';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import React, { useState } from 'react';
 
 export default function ReturnItemScreen() {
-  const colorScheme = useColorScheme();
+  const { reservationId } = useLocalSearchParams<{ reservationId: string }>();
+  if (!reservationId) throw new Error('Missing reservationId');
+  const [scanned, setScanned] = useState(false);
+  const [scannedValue, setScannedValue] = useState('');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) return <View />;
+
+  if (!permission.granted) {
+      return (
+          <View style={styles.container}>
+              <Text style={{ textAlign: 'center' }}>
+                  Requires permission to access the camera
+              </Text>
+              <Button onPress={requestPermission} title='grant permission' />
+          </View>
+      );
+  }
+
+  const handleBarCodeScanned = ({ data }: { data: string }) => {
+    if (data) {
+        setScanned(true);
+        setScannedValue(data);
+    }
+  };
+
+  const handleResult = () => {
+    if (scannedValue) {
+        try {
+            const { verifyToken } = JSON.parse(scannedValue);
+            console.log(verifyToken);
+            return (
+                <Text style={styles.resulttext}>
+                    {'Successfully, verified returning item'}
+                </Text>
+            );
+        } catch (e) {
+            return (
+                <Text style={styles.resulttext}>
+                    {'Invalid QR code. Please scan a valid QR code'}
+                </Text>
+            );
+        }
+    }
+    return <Text style={styles.resulttext}>No QR code scanned</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Return Item</Text>
-      <Text style={styles.title}>QR Scan</Text>
-    </View>
+    <BackgroundLayout>
+    <MainHeader title="Borrowed Items" />
+    <ClerkReservationsHorizontalBar selectedIndex = {1} />
+    <ContentContainer>
+      <View style={styles.container}>
+        <View style={styles.camerabox}>
+                    <CameraView
+                        barcodeScannerSettings={{
+                            barcodeTypes: [
+                                'aztec',
+                                'ean13',
+                                'ean8',
+                                'qr',
+                                'pdf417',
+                                'upc_e',
+                                'datamatrix',
+                                'code39',
+                                'code93',
+                                'itf14',
+                                'codabar',
+                                'code128',
+                                'upc_a',
+                            ],
+                        }}
+                        onBarcodeScanned={
+                            scanned ? undefined : handleBarCodeScanned
+                        }
+                        style={StyleSheet.absoluteFillObject}
+                    />
+                    {scanned && (
+                        <Button
+                            title={'Tap to Scan Again'}
+                            onPress={() => setScanned(false)}
+                        />
+                    )}
+        </View>
+        <View style={styles.textbox}>
+          {handleResult()}
+        </View>
+        <ContentContainerHeader title="Verify Returning Item" />
+      </View>
+    </ContentContainer>
+    </BackgroundLayout>
   );
 }
 
@@ -19,6 +109,8 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'transparent',
+    width: '100%',
   },
   title: {
     fontSize: 20,
@@ -29,4 +121,47 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+  camerabox: {
+    flex: 1,
+    justifyContent: 'center',
+    width: '100%',
+  },
+  resultlink: {
+      color: 'blue',
+      flexWrap: 'wrap',
+      fontFamily: 'monospace',
+      padding: 20,
+      textDecorationLine: 'underline',
+  },
+  resulttext: {
+      color: 'white',
+      flexWrap: 'wrap',
+      fontFamily: 'monospace',
+      padding: 20,
+  },
+  textbox: {
+      borderTopColor: 'blue',
+      borderTopWidth: 3,
+      alignItems: 'center',
+      flex: 1,
+      justifyContent: 'center',
+      width: '100%',
+  }
 });
