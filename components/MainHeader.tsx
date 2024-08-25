0766adbc9513cc488b146000dfce6328e69920e3
Sharada Shehan <sharadashehan6@gmail.com>
2024-08-25 12:05:38 +0530

    back button added for navigation

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,20 +1,28 @@
 import { StyleSheet, Pressable } from 'react-native';
-import { Link, router } from 'expo-router';
+import { Link, router, usePathname, useSegments } from 'expo-router';
 import { Text, View } from '@/components/Themed';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 
 const MainHeader: React.FC<{ title: string }> = ({ title }) => {
+  const segments = useSegments();
+  const fullPath = '/' + segments.join('/');
+  console.log(fullPath);
   return (
-    <View style={{ 'backgroundColor': 'transparent', 'width': '100%', 'alignItems': 'center' }}>
+    <View style={{ backgroundColor: 'transparent', width: '100%', alignItems: 'center' }}>
       <View style={styles.separator}></View>
-      <View style={{ 'flexDirection': 'row', 'justifyContent': 'center', 'alignItems': 'center', backgroundColor: 'transparent', 'width': '90%' }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: 'transparent', width: '90%' }}>
+        <Pressable onPress={() => router.back()} style={{ position: 'absolute', left: 0 }}>
+          {({ pressed }) => (
+            <FontAwesome6 name="arrow-left" size={26} color="white" style={{ opacity: pressed ? 0.5 : 1 }} />
+          )}
+        </Pressable>
         <Text style={styles.headerText}>{title}</Text>
-        <Link href="/profile" asChild style={{ 'position': 'absolute', 'right': 0 }}>
+        <Link href="/profile" asChild style={{ position: 'absolute', right: 0 }}>
           <Pressable>
             {({ pressed }) => (
-              <FontAwesome6 name="user-circle" size={28} color="white" style={{ 'opacity': pressed ? 0.5 : 1 }} />
-              )}
+              <FontAwesome6 name="user-circle" size={28} color="white" style={{ opacity: pressed ? 0.5 : 1 }} />
+            )}
           </Pressable>
         </Link>
       </View>
